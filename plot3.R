92ebf787572044ab9c160cb0e374e4bcecb385e7
joshi-v <vdashj@gmail.com>
2017-03-11 21:56:04 -0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -16,8 +16,8 @@ with(elec.sub, {
   lines(Sub_metering_3~Datetime,col='Blue')
 })
 
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, 
-       lwd=2.5, col=c("black", "red", "blue"))
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       lty=1, lwd = 2.5, col=c("black", "red", "blue"), cex = 0.75)
 
 dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
